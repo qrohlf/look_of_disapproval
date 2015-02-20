2308a4c2af64a2b34e6e44e539328d59343fe35b
Quinn Rohlf <qr@qrohlf.com>
2015-02-20 11:51:05 -0800

    Really jank emoji support

--- lib/look_of_disapproval.rb
+++ lib/look_of_disapproval.rb
@@ -2,9 +2,10 @@ require "look_of_disapproval/version"
 
 module LookOfDisapproval
   class Disapprover
-    def initialize(message=nil)
+    def initialize(message=nil, emoji=nil)
       @template = ERB.new(File.read(File.dirname(__FILE__)+'/look_of_disapproval/disapprove.erb'))
       @message = message
+      @emoji = emoji
     end
 
     def to_html
--- lib/look_of_disapproval/version.rb
+++ lib/look_of_disapproval/version.rb
@@ -1,3 +1,3 @@
 module LookOfDisapproval
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
